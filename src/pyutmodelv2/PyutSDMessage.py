6ed16af691fd3e523dc95e919435f8f4e2dc7455
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2025-01-24 15:33:48 -0700

    <>[]: <Fix PyutSDMessage>
    [
    And remove requirements.txt
    ]
    
    [
    https://github.com/hasii2011/pyutmodelv2/issues/12
    https://github.com/hasii2011/pyutmodelv2/issues/11
    ]

--- src/pyutmodelv2/PyutSDMessage.py
+++ src/pyutmodelv2/PyutSDMessage.py
@@ -5,6 +5,7 @@ from logging import getLogger
 from dataclasses import dataclass
 
 from pyutmodelv2.PyutLink import PyutLink
+from pyutmodelv2.enumerations.PyutLinkType import PyutLinkType
 
 
 @dataclass
@@ -13,7 +14,7 @@ class PyutSDMessage(PyutLink):
     sourceY:      int = 0
     destinationY: int = 0
     """
-    A message between two lifeline of two SDInstances.
+    A message lifeline between two SDInstances.
 
     """
     def __init__(self, message: str = "", src=None, sourceY: int = 0, dst=None, destinationY: int = 0):
@@ -30,61 +31,11 @@ class PyutSDMessage(PyutLink):
         self.logger: Logger = getLogger(__name__)
 
         self.logger.debug(f"PyutSDMessage.__init__ {sourceY}, {destinationY}")
-        super().__init__(source=src, destination=dst)
+        super().__init__(source=src, destination=dst, linkType=PyutLinkType.SD_MESSAGE)
 
         self.message      = message
         self.sourceY      = sourceY
         self.destinationY = destinationY
-
-    # @property
-    # def sourceId(self) -> int:  # ignore it because the default is None
-    #     return self._src.id     # type: ignore
-    #
-    # @property
-    # def destinationId(self) -> int:
-    #     return self._dest.id      # type: ignore
-    #
-    # def getSource(self):
-    #     """
-    #     Return Y position on source
-    #     @author C.Dutoit
-    #     """
-    #     return self._src
-    #
-    # def getDest(self):
-    #     """
-    #     Return Y position on source
-    #     @author C.Dutoit
-    #     """
-    #     return self._dest
-    #
-    # def setSource(self, src=None, srcTime=-1):
-    #     """
-    #     Args:
-    #         src:    Source object
-    #         srcTime: ???
-    #     """
-    #     if src is not None:
-    #         super().setSource(src)
-    #     if srcTime != -1:
-    #         self.logger.debug(f"PyutSDMessage - Setting srcTime to: {srcTime}")
-    #         # self.setSrcTime(srcTime)
-    #         self.sourceY = srcTime
-    #
-    # def setDestination(self, dst=None, dstTime=-1):
-    #     """
-    #     Define the destination
-    #
-    #     Args:
-    #         dst:        destination object
-    #         dstTime:    Time on the destination
-    #     """
-    #     if dst is not None:
-    #         PyutLink.setDestination(self, dst)
-    #     if dstTime != -1:
-    #         self.logger.debug(f"Setting dstTime to {dstTime}")
-    #         # self.setDstTime(dstTime)
-    #         self.destinationY = dstTime
 
     def __str__(self):
         """
